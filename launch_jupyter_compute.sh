a36d690b5b6252e696dfae23c524c6429599e26b
Michele Mesiti <michele.mesiti@gmail.com>
2020-04-24 14:09:06 +0100

    Added explicit --oversubscribe option to sbatch command

--- launch_jupyter_compute.sh
+++ launch_jupyter_compute.sh
@@ -1,6 +1,6 @@
 # Compute node
 LOG=sunpyter_log.txt
 touch $LOG
-sbatch --partition development -A scw1000 -o $LOG -J SUNPYTER -n 1 jupyter notebook --no-browser --ip='*'
+sbatch --partition development -A scw1000 -o $LOG -J SUNPYTER -n 1 --oversubscribe jupyter notebook --no-browser --ip='*'
 tail -f $LOG
 
