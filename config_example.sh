390f8977b8b5bdd6fec5dcfc5c18591a73868798
Tianyi <97114518+TummyPan@users.noreply.github.com>
2022-09-13 18:08:33 +0100

    Update config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -1,5 +1,4 @@
-REMOTE=s.michele.mesiti@vnc.sunbird.swansea.ac.uk # Username&host, or alias
-SHELLSETUP=shellsetup_example.sh # see example
+REMOTE=s.tianyi.pan@sunbird.swansea.ac.uk # Username&host, or alias
+SHELLSETUP=shellsetup_example.sh
 RUNWHERE=compute # either 'compute' or 'login'
 ACCOUNT=scw1000 # account to run the job on for the jupyter notebook.
-
