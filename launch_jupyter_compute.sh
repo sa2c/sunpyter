8e0f3c6b807bfc6f6b47e96c2c7a65e1bea855e7
Tianyi <97114518+TummyPan@users.noreply.github.com>
2022-09-13 18:09:22 +0100

    Update launch_jupyter_compute.sh

--- launch_jupyter_compute.sh
+++ launch_jupyter_compute.sh
@@ -3,6 +3,5 @@ LOG=~/sunpyter_log.txt
 rm -r $LOG
 touch $LOG
 chmod go-r $LOG
-sbatch --partition development -A SEDACCOUNT -o $LOG -J SUNPYTER --dependency=singleton -n 1 --oversubscribe jupyter notebook --no-browser --ip='*'
+sbatch --partition accel_ai -A SEDACCOUNT -o $LOG -J SUNPYTER --dependency=singleton -n 1 --gres=gpu:1 --oversubscribe jupyter notebook --no-browser --ip='*'
 tail -f $LOG
-
