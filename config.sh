507455915993e3a11a38298eef67aeafd2e597bb
TummyPan <Dr.Tianyi.Pan@outlook.com>
2022-09-14 13:54:28 +0100

    configured for gpu on Sunbird

---
+++ config.sh
@@ -0,0 +1,4 @@
+REMOTE=s.tianyi.pan@sunbird.swansea.ac.uk # Username&host, or alias
+SHELLSETUP=shellsetup.sh
+RUNWHERE=compute # either 'compute' or 'login'
+ACCOUNT=scw1000 # account to run the job on for the jupyter notebook.
---
+++ shellsetup.sh
@@ -0,0 +1,9 @@
+# This file contains the commands to set up the jupyter notebook environment
+# correctly.
+# 1 activation of a conda environment;
+# 2 choosing a directory where to start the jupyter notebook;
+
+# This is a conda environment containing a jupyter notebook installation
+source activate /scratch/s.tianyi.pan/jupyter_gpu
+# working directory
+cd /lustrehome/home/s.tianyi.pan
