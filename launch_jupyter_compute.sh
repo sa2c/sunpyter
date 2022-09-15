e42b248a515d098a108b6a0d7ca66285384780a0
Tianyi <97114518+TummyPan@users.noreply.github.com>
2022-09-15 14:39:19 +0100

    Update launch_jupyter_compute.sh

--- launch_jupyter_compute.sh
+++ launch_jupyter_compute.sh
@@ -3,5 +3,6 @@ LOG=~/sunpyter_log.txt
 rm -r $LOG
 touch $LOG
 chmod go-r $LOG
+# If you need to use GPU, specify --partition as accel_ai and include --gres=gpu:n (n is number of GPUs)
 sbatch --partition accel_ai -A SEDACCOUNT -o $LOG -J SUNPYTER --dependency=singleton -n 1 --gres=gpu:1 --oversubscribe jupyter notebook --no-browser --ip='*'
 tail -f $LOG
