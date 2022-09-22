29892a41ba84e03509e745fce93835330a3d81ca
TummyPan <Dr.Tianyi.Pan@outlook.com>
2022-09-22 09:48:54 +0100

    remove personal set-up

--- config.sh
+++
@@ -1,6 +0,0 @@
-### Please Note ###
-# This is the set-up for my SCW accout, please modify config_example.sh for your use
-REMOTE=s.tianyi.pan@sunbird.swansea.ac.uk # Username&host, or alias
-SHELLSETUP=shellsetup.sh
-RUNWHERE=compute # either 'compute' or 'login'
-ACCOUNT=scw1000 # account to run the job on for the jupyter notebook.
