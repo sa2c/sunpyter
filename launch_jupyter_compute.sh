e208a1e2db6337d470dd7d84efaf2c75015aa4ac
TummyPan <Dr.Tianyi.Pan@outlook.com>
2022-10-12 10:17:55 +0100

    update sunpyter.sh to avoid using two terminals

--- launch_jupyter_compute.sh
+++ launch_jupyter_compute.sh
@@ -4,5 +4,5 @@ rm -r $LOG
 touch $LOG
 chmod go-r $LOG
 # If you need to use GPU, specify --partition as accel_ai and include --gres=gpu:n (n is number of GPUs)
-sbatch --partition accel_ai -A SEDACCOUNT -o $LOG -J SUNPYTER --dependency=singleton -n 1 --gres=gpu:1 --oversubscribe jupyter notebook --no-browser --ip='*'
+sbatch --partition accel_ai -A SEDACCOUNT -o $LOG -J SUNPYTER_$USER --dependency=singleton -n 1 --gres=gpu:1 --oversubscribe jupyter notebook --no-browser --ip='*'
 tail -f $LOG
