899bf91e32522f5f0d02ffda6ba970eb1bc83684
TummyPan <Dr.Tianyi.Pan@outlook.com>
2022-10-18 12:15:36 +0100

    add function to select CPU or GPU

--- remote_script.sh
+++ remote_script.sh
@@ -1,7 +1,24 @@
-#!/bin/bash 
-ACCOUNT=scw1000
-# This is a conda environment containing a jupyter notebook installation
+#!/bin/bash
+# Please speficy your SCW project account (for example, scwXXXX). 
+ACCOUNT=scw1000 # This is an example of SCW account.
+
+# Please speficy the partition for running your Jupyter notebooks.
+# If you need CPU then use "compute". If you need GPU, please speficy "accel_ai".
+PARTITION=accel_ai # Must be either "compute" or "accel_ai"
+
+# For "compute" ONLY! 
+# Please speficy the number of CPU cores you need.
+NUM_CPU=4 # This is the default setting (4 cores).
+
+# For "accel_ai" ONLY! 
+# Please speficy the number of GPUs you need.
+NUM_GPU=1 # This is the default setting (1 GPU).
+
+# This is a very simple conda environment containing a jupyter notebook installation
+# Please speficy your own customised conda environment if you need.
 CONDA_ENV_PATH=/scratch/s.tianyi.pan/jupyter_gpu
+
+# Please specify the working directory for your Jupyter notebooks.
 WORKDIR=/home/$USER
 
 export JUPYTER_CONFIG_DIR=$CONDA_ENV_PATH/etc/jupyter
@@ -40,16 +57,34 @@ else
 fi
 SCRIPT_CONTENT
 
-sbatch -A $ACCOUNT \
-    --partition accel_ai \
-    -o $LOG \
-    -J SUNPYTER_$USER \
-    --dependency=singleton \
-    -n 1 \
-    --gres=gpu:1 \
-    --oversubscribe \
-    job_script_sunpyter.sh
+if [ "$PARTITION" == "compute" ]
+then
+    sbatch -A $ACCOUNT \
+        --partition=compute \
+        -o $LOG \
+        -J SUNPYTER_$USER \
+        --dependency=singleton \
+        -n 1 \
+        --ntasks=&NUM_CPU \
+        --oversubscribe \
+        job_script_sunpyter.sh
+    
+elif [ "$PARTITION" == "accel_ai" ]
+then
+    sbatch -A $ACCOUNT \
+        --partition=accel_ai \
+        -o $LOG \
+        -J SUNPYTER_$USER \
+        --dependency=singleton \
+        -n 1 \
+        --gres=gpu:$NUM_GPU \
+        --oversubscribe \
+        job_script_sunpyter.sh
 
+else
+    echo 'Wrong launch specification, use either "compute" or "accel_ai".'
+    exit
+fi
 # This is used to sends the output of the log
 # to the user's machine for scraping 
 tail -f $LOG 
